1f4dffecbdc09d362b7a612ec088deb1ed26e711
Alejandro Cruz <daniel_719021@hotmail.es>
2023-12-08 15:01:55 -0600

    first commit

---
+++ auth/src/models/user.ts
@@ -0,0 +1,46 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+import { Password } from "../services/password";
+
+// An interface that describes the properties that are required to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties that a User Document has
+interface UserDoc extends Document, UserAttrs { }
+
+// An interface that describes the properties that a User Model has
+interface UserModel extends Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties that a User Document has
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function(done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+
+    done();
+  }
+});
+
+userSchema.statics.build = (attrs: UserAttrs): UserDoc => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
